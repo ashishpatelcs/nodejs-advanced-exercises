d74b2a582afd1ec29321f131f2ac90120a508cb9
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-16 09:18:45 +0530

    fiddling with module, exports and require functions

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const readfs = require('./readFile')
 const writefs = require('./writeFile')
 
 // readfs.readDirectoryAsynchronously()
-
+// console.log(writefs.writeFileSynchronously.toString())
 writefs.writeFileAsynchronously()
+console.log(module)
+console.log(exports)
+console.log(require.toString());
+console.log(mod)
+//console.log(__filename.);
+//console.log(__dirname)
--- writeFile.js
+++ writeFile.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
+let x = 1000000
 
 let writeFileSynchronously = () => {
-    let data = Math.random()*1000000
+    let data = Math.random()*x
     fs.writeFileSync('write.txt', data)
     console.log('file written');
 }
 
 let writeFileAsynchronously = () => {
-    let data = Math.random()*1000000
+    let data = Math.random()*x
     fs.writeFile('write.txt', data, (err) => {
         if (err) console.log(err);
         else console.log('file written!');
